12e443f0dba0064911a13c8e6b3db7f38ea6c018
Kateryna Serhiienko <kate.serhiienko@gmail.com>
2024-06-14 14:40:15 +0200

    add: example photos, slider and styles in team component

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,33 +1,83 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const Team = () => {
+const Team = ({ teamMembers }) => {
+  const sliderSettings = {
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <section>
-      Team
-      <h2>Our Team</h2>
-      <p>Team description</p>
-      <div>
-        {/* teamGallery */}
-        <ul>
-          <li>
-            <img src="" alt="" />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-          <li>
-            <img src="" alt="" />
-          </li>
-        </ul>
-      </div>
+    <section className="external-container py-8">
+      <h2 className="mb-4 text-center font-accent tracking-widest font-bold">
+        <p className="text-lg text-red-600">TEAM</p>
+        <p className="text-4xl text-main-dark">FREIBERGER MAULWÜRFE</p>
+      </h2>
+      <p className="mb-4">
+        Our team is comprised of a dynamic group of players, coaches, and
+        support staff who are passionate about volleyball. Whether it&apos;s
+        spiking, blocking, or strategizing, each member brings their unique
+        skills and energy to the court. Meet our amazing team and get to know
+        the individuals who make every game a thrilling experience. Stay tuned
+        for updates on our upcoming matches and events!
+      </p>
+      <Slider {...sliderSettings}>
+        {teamMembers.map((member, index) => (
+          <div key={index}>
+            <div className="w-56 h-auto mx-auto bg-slate-100 rounded overflow-hidden text-center">
+              <div
+                className="w-full h-72 mb-4 bg-cover bg-top bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${member.img})`,
+                }}
+              ></div>
+              <h3 className="font-bold">{member.name}</h3>
+              <p className="mb-4 text-sm">
+                {member.position && `${member.position}`}
+              </p>
+            </div>
+          </div>
+        ))}
+      </Slider>
     </section>
   );
+};
+
+Team.propTypes = {
+  teamMembers: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      position: PropTypes.string,
+      img: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Team;

552fc5d62563ce5605395e69de5fa79a23ac37f0
Kateryna Serhiienko <kate.serhiienko@gmail.com>
2024-06-19 00:29:59 +0200

    fix: paddings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import logo from '../assets/images/logo-full.png';
 const Footer = () => {
   return (
     <div className="bg-main-dark text-white/40">
-      <div className="external-container py-10 sm:py-16 lg:py-20">
+      <div className="external-container pb-8 pt-10 sm:pt-16 lg:pt-20">
         <div className="flex flex-col items-center justify-between gap-8 md:flex-row md:gap-28">
           <ContactUsForm />
           <img
@@ -14,7 +14,7 @@ const Footer = () => {
             alt="Freiberger Maulwürfe"
           />
         </div>
-        <div className="flex flex-col items-center justify-between gap-2 mt-4 pt-4 border-t-2 border-white/10 md:flex-row">
+        <div className="flex flex-col items-center justify-between gap-2 mt-8 pt-8 border-t-2 border-white/10 md:flex-row">
           <NavMenu />
           <p>© 2024 Freiberger Maulwürfe. All rights reserved.</p>
         </div>
